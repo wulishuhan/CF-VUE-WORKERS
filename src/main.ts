6ce78d45d72de90d24a8acd327a799be370bb07a
shuhan <438115215@qq.com>
2022-06-25 15:33:12 +0800

    vite
    vue3

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// import store from './store'
-// import router from '@/router';
-// 创建vue实例
-const app = createApp(App)
 
-// 挂载pinia
-// app.use(store)
-// app.use(router);
-// 挂载实例
-app.mount('#app');
+createApp(App).mount('#app')
